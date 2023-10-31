60c1ed17485e7c0510aff308b637b859d8dda595
Lafhil <lafhil@gmail.com>
2023-10-31 11:25:13 +0100

    template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {MenuItem} from "primeng/api";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'eCommerceFrontEnd';
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: '<span class="text-xl font-bold">Refresh</span>',
+            escape: false,
+            icon: 'pi pi-refresh',
+            iconClass: 'text-xl'
+          },
+          {
+            label: '<span class="text-xl font-bold">Delete</span>',
+            escape: false,
+            icon: 'pi pi-times',
+            iconClass: 'text-xl'
+          }
+        ]
+      },
+      {
+        label: 'Navigate',
+        items: [
+          {
+            label: 'Angular',
+            icon: 'pi pi-external-link',
+            url: 'http://angular.io'
+          },
+          {
+            label: 'Router',
+            icon: 'pi pi-upload',
+            routerLink: '/fileupload'
+          }
+        ]
+      }
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppComponent } from './app.component';
+import { FooterComponent } from './template/footer/footer.component';
+import { HeaderComponent } from './template/header/header.component';
+import { NavBarComponent } from './template/nav-bar/nav-bar.component';
+import {ProductFormComponent} from "./CompenentsCore/product-form/product-form.component";
+import { SlideMenuModule } from "primeng/slidemenu";
+import { ButtonModule } from "primeng/button";
+import {MegaMenuModule} from "primeng/megamenu";
+import {MenuModule} from "primeng/menu";
+import {BadgeModule} from "primeng/badge";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavBarComponent,
+
+    ProductFormComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SlideMenuModule,
+    ButtonModule,
+    MegaMenuModule,
+    MenuModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
